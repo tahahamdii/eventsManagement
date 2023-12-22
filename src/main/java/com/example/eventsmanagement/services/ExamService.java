26e7e9aab4dc415ad8fc3d24f889ab8e732ac2c5
Med Taha Hamdi <hamdi.taha@esprit.tn>
2023-12-22 19:09:22 +0100

    1)a/b

--- src/main/java/com/example/eventsmanagement/services/ExamService.java
+++ src/main/java/com/example/eventsmanagement/services/ExamService.java
@@ -1,7 +1,29 @@
 package com.example.eventsmanagement.services;
 
+import com.example.eventsmanagement.entity.Activite;
+import com.example.eventsmanagement.entity.Evenement;
+import com.example.eventsmanagement.entity.Utilisateur;
+import com.example.eventsmanagement.repositories.ActiviteRepo;
+import com.example.eventsmanagement.repositories.EvenementRepo;
+import com.example.eventsmanagement.repositories.UtilisateurRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ExamService {
+public class ExamService implements ExamServiceImpl{
+    @Autowired
+    private EvenementRepo evenementRepo;
+    @Autowired
+    private UtilisateurRepo utilisateurRepo;
+    @Autowired
+    private ActiviteRepo activiteRepo;
+    @Override
+    public Evenement addEvenement(Evenement e) {
+        return evenementRepo.save(e);
+    }
+
+    @Override
+    public Activite addActivite(Activite a) {
+        return activiteRepo.save(a);
+    }
 }
--- src/main/java/com/example/eventsmanagement/services/ExamServiceImpl.java
+++ src/main/java/com/example/eventsmanagement/services/ExamServiceImpl.java
@@ -1,4 +1,9 @@
 package com.example.eventsmanagement.services;
 
+import com.example.eventsmanagement.entity.Activite;
+import com.example.eventsmanagement.entity.Evenement;
+
 public interface ExamServiceImpl {
+ public Evenement addEvenement(Evenement e);
+ public Activite addActivite(Activite a);
 }
